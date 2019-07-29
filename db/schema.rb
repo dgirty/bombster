9095b62e73762956590ed68079031ce0b986d9e8
dgirty <dggirty@gmail.com>
2019-07-29 21:35:23 +0000

    Bombster39----

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2019_07_28_185548) do
   end
 
   create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "user_id"
+    t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
